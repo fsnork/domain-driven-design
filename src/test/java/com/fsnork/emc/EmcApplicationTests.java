11167c49db2bdee174e1a1f6a81c40aef1f0fc8a
fsnork <m@ej3.ru>
2021-09-05 21:55:27 +0300

    HelloController, Test Commit

---
+++ src/test/java/com/fsnork/emc/EmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsnork.emc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
